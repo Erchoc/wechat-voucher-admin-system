9f2a6e047a1fc1dc90a67b626bc15ab0090d9fbb
X.C <119136016@qq.com>
2016-03-20 13:26:39 +0800

    上传logo
    上传logo

---
+++ dao/CardDao.js
@@ -0,0 +1,35 @@
+﻿var Promise = require('bluebird');
+//数据操作层
+module.exports = Card = function () { };
+
+
+/**
+ * Logo地址入库
+ * param item logo数据(JSON)
+ **/
+Card.prototype.insertLogo = function (item) {
+    return new Promise(function (resolve, reject) {
+        db.models['logo'].create(item, function (err, item) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(item);
+        })
+    })
+}
+
+/**
+ *  获取logo列表
+ **/
+ Card.prototype.logoList = function () {
+    return new Promise(function (resolve, reject) {
+        db.models['logo'].find({}, function (err, logos) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(item);
+        })
+    })
+}
---
+++ model/logoModel.js
@@ -0,0 +1,9 @@
+﻿//定义表结构Model
+module.exports = function (db, cb) {
+    //define方法的第一个参数为表名
+    db.define('logo', {
+        id: { type: 'serial', key: true } , //主键
+        url: String
+    });
+    return cb();
+}
