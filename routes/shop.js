15116bc47dc411a7ee5f88e6a2affd882cf157c5
X.C <119136016@qq.com>
2016-04-03 16:16:26 +0800

    新增门店

---
+++ routes/shop.js
@@ -0,0 +1,27 @@
+﻿/**
+ * 门店路由
+ */
+var Shop = require('../dao/shopDao');
+var shop = new Shop();
+
+module.exports.autoroute = {
+    get: {
+        '/shop/shopList/query' : getShopList,  //获取门店列表
+    },
+    post: {
+    }
+};
+
+/**
+ *  获取门店列表
+ **/
+function getShopList(req, res) {
+    shop.getShopList()
+    .then(function (shops) {
+        res.jsonp({ status: 200, msgBody: shops });
+    })
+    .catch(function (err) {
+        console.error(err);
+        res.jsonp({ status: -1, msgBody: err });
+    })
+}
